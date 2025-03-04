9f0a73e25f2b4c6d711b7337fd10f50f698124f2
AndiRrahmani <andirrahmani28@gmail.com>
2025-03-04 16:15:42 +0100

    Module 9

---
+++ module 9/config.php
@@ -0,0 +1,16 @@
+<?php
+$server = 'localhost';
+$username = 'root';
+$dbname = 'db';
+$password= '';
+
+
+try {
+    $connection = new PDO("mysql:host=$server;dbname=$dbname;", $username, $password);
+
+} catch(Exception $e){
+    echo "Something went wrong";
+}
+
+
+?>
