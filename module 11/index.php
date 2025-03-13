703b48e3e9c6d03a65ca42455b4d4b41a066d40a
AndiRrahmani <andirrahmani28@gmail.com>
2025-03-13 16:12:45 +0100

    Module 11

---
+++ module 11/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+
+<form action="add.php" method="POST">
+
+<label for="name">Name:</label>
+<input type="text" id="name" name="name" placeholder="Name"><br>
+<label for="surname">Surname:</label>
+<input type="surname" id="surname" name="surname" placeholder="Surname"><br>
+<label for="email">Email:</label>
+<input type="email" id="email" name="email" placeholder="Email"><br>
+<label for="age">Age:</label>
+<input type="age" id="age" name="age" placeholder="Age"><br>
+<button type="submit" name="submit">Add</button>
+</form>
+
+
+    
+</body>
+</html>
