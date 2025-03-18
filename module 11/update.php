cce9bd7bd95251c89945bbcf95053b36e38dc5e4
AndiRrahmani <andirrahmani28@gmail.com>
2025-03-18 16:20:31 +0100

    Module 11

---
+++ module 11/update.php
@@ -0,0 +1,16 @@
+<?php
+include_once('config.php');
+
+if(isset($_POST['update']))
+{
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $age = $_POST['age'];
+
+
+}
+
+
+?>
